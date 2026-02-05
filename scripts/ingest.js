c45b8a4d2820a2438929c8ae5e0ab9a2b8c425c9
Don Wea <donwea@Dons-Mac-mini.local>
2026-02-05 11:07:26 -0300

    fix(lint): avoid require() in ingest runner

--- scripts/ingest.js
+++ scripts/ingest.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
-// CommonJS runner for the compiled ingest code under dist/
-// This avoids ESM extension/path-alias issues when executing in Node.
-
-const { runIngest } = require('../dist/scripts/ingest-runner.js');
+// ESM runner for the compiled ingest code under dist/
+// Using dynamic import avoids CJS require() lint restrictions.
 
 const dryRun = process.argv.includes('--dry');
 
-(async () => {
-  const res = await runIngest({ dryRun });
-  console.log(JSON.stringify(res, null, 2));
-  if (!res.ok) process.exit(1);
-})();
+const mod = await import('../dist/scripts/ingest-runner.js');
+const { runIngest } = mod;
+
+const res = await runIngest({ dryRun });
+console.log(JSON.stringify(res, null, 2));
+if (!res.ok) process.exit(1);
